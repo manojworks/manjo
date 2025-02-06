157ae8e5331f718db6a6f0aaf65ae2f5f286cbd9
Manoj Saxena <42760785+manojworks@users.noreply.github.com>
2025-02-05 18:39:38 -0600

    basic models for tracks

--- client/webapp/src/app/models/track.ts
+++ client/webapp/src/app/models/track.ts
@@ -2,6 +2,16 @@ import { Singer } from './singer';
 import { Composer } from './composer';
 import { Writer } from './writer';
 
+export interface TrackItem {
+    id: number;
+    title: string;
+    composers: string;
+    singers: string;
+    writers: string;
+    releaseYear: number;
+    description: string;
+}
+
 export interface Track {
     id: number;
     title: string;
