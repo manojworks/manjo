f4655b955302efed9d4327fb674b37bcafb22999
Manoj Saxena <42760785+manojworks@users.noreply.github.com>
2025-02-11 15:07:39 -0600

    add attribute selection dropdown

--- client/webapp/src/app/components/search/search.component.ts
+++ client/webapp/src/app/components/search/search.component.ts
@@ -1,8 +1,11 @@
-import { Component , OnInit, Output, EventEmitter} from '@angular/core';
+import { Component , OnInit, Output, EventEmitter, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {RouterModule } from '@angular/router';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule, MatTable} from '@angular/material/table';
+import { MatFormField } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
 import { ActivatedRoute } from '@angular/router';
 import { TrackService } from '../../services/track.service';
 import { CommonModule } from '@angular/common'
@@ -11,7 +14,7 @@ import { TrackItem } from '../../models/track';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, RouterModule, MatTableModule, CommonModule],
+  imports: [FormsModule, RouterModule, MatTableModule, CommonModule, MatSelectModule, MatOptionModule, ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
   animations: [
@@ -79,5 +82,19 @@ export class SearchComponent implements OnInit {
   
     public showExpandedElement(element: TrackItem): void {
       this.expandedTracks.push(element);
+    }
+
+    trackElems: string[] = ['all', 'title', 'singer', 'composer',  'writer', 'lyrics', 'actor'];
+    trackAttributeSelectedValue: string = this.trackElems[0];
+
+
+    clickHandler(item: string) {
+      this.trackAttributeSelectedValue = item;
+      this.searchText = this.trackAttributeSelectedValue;
+      
+    }
+
+    @HostListener('click') hostClick() {
+      this.searchText = this.trackAttributeSelectedValue;
     }
 }
